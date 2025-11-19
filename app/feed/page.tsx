12bff3403e01b57e7b28d63d4b1b1ee4b5d27cd5
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-11-18 14:15:24 -1100

    Create page.tsx

---
+++ app/feed/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Feed from "../components/Feed";
+
+export default function FeedPage() {
+  return (
+    <div className="h-full bg-gray-50 dark:bg-neutral-900">
+      <Feed />
+    </div>
+  );
+}
